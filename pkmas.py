f559143d9eecdb70684096df6c1abf9f30b29c46
ltanaka01 <51801154+ltanaka01@users.noreply.github.com>
2019-09-19 15:32:56 -0400

    Add files via upload

---
+++ pkmas.py
@@ -0,0 +1,17 @@
+import os, glob, fnmatch, re
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+# data = pd.read_csv("4004_analyzed.csv")
+# data.head()
+
+import csv
+with open('4004_analyzed.csv') as f:
+	reader = csv.reader(f)
+	#for row in reader: 
+	#	print(row)
+
+
+
+
